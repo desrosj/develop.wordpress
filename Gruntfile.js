86c49002119edcf82d5b9fc531f826d81537cfac
Jonathan Desrosiers <desrosj@gmail.com>
2017-04-25 22:35:33 -0400

    Testing patch for 40549 on the 3.8 branch.

--- Gruntfile.js
+++ Gruntfile.js
@@ -384,6 +384,7 @@ module.exports = function(grunt) {
 	grunt.registerTask('qunit:compiled', 'Runs QUnit tests on compiled as well as uncompiled scripts.',
 		['build', 'copy:qunit', 'qunit']);
 	grunt.registerTask('test', 'Runs all QUnit and PHPUnit tasks.', ['qunit:compiled', 'phpunit']);
+	grunt.registerTask('travis', ['jshint', 'test']);
 
 	// Default task.
 	grunt.registerTask('default', ['build']);
